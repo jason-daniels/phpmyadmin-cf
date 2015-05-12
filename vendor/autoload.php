42f44fc92db0174f559ad9841adac6b91c462b75
Arthur Halet <arthur.halet@orange.com>
2015-05-12 13:54:51 +0200

    Update dependencies

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInite40889f645b8296515b3d93ebd587ac9::getLoader();
+return ComposerAutoloaderInit5b7f1507d975105839d653f3a0db2918::getLoader();
